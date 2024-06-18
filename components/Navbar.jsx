8cd0a36c2d47f520b67675dfd0f470a156025703
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-06-18 21:31:18 +0530

    navbar style fix with daisyUI

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,24 +3,22 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <header className="">
-      <div className="container bg-base-300 mx-auto py-6 px-4">
-        <nav className="flex items-center justify-between space-x-20">
-          <Link href="/">
-            <div className="flex items-center">
-              <Image src="/logo.svg" alt="Logo" width={32} height={32} className="mx-2" />
-              <span className="font-semibold text-xl text-primary">jsProdigy</span>
-            </div>
+    <header className="bg-base-100 shadow-md">
+      <div className="container mx-auto navbar">
+        <div className="navbar-start">
+          <Link href="/" className="flex items-center">
+            <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+            <span className="ml-2 text-2xl font-semibold text-primary">jsProdigy</span>
           </Link>
-          <div className="flex space-x-8">
-            <Link href="/login">
-              <button className="btn btn-ghost">Login</button>
-            </Link>
-            <Link href="/signup">
-              <button className="btn btn-primary">Sign Up</button>
-            </Link>
-          </div>
-        </nav>
+        </div>
+        <div className="navbar-end">
+          <Link href="/login" className="btn btn-ghost btn-sm rounded-btn">
+            Login
+          </Link>
+          <Link href="/signup" className="btn btn-primary btn-sm rounded-btn ml-2">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </header>
   );
