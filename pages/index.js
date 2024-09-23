399eaeb0102998f7b4b41f0b81b37dd4849dc8e8
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-09-23 01:35:17 -0400

    date change

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
 							<div>
 								<div className="mb-8">
 									<p className="text-2xl font-semibold mb-4">
-										New Batch Starting from September 20th!
+										New Batch Starting from October 1st!
 									</p>
 									<ul className="list-disc pl-8 text-lg space-y-2">
 										<li>Live Classes every Monday, Wednesday & Friday.</li>
